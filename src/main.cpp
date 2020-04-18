43a72dbc234cddbf3cb6c8f6c7c2eb04e08d134a
jonathon <jonathonwhite@protonmail.com>
2020-04-18 19:43:31 +0200

    click and drag works

--- src/main.cpp
+++ src/main.cpp
@@ -37,11 +37,21 @@ struct Application
   unsigned int H;
   sf::Texture* texture;
   sf::Sprite* sprite;
+  int mouseDragging = 0;
+  int center_col;
+  int center_col_delta;
+  int center_row;
+  int center_row_delta;
+  int mouse_down_col;
+  int mouse_down_row;
 
   Application()
   {
     W = 200;
     H = 200; // you can change this to full window size later
+
+    center_row = H/2;
+    center_col = W/2;
 
     // sf::RenderWindow window(sf::VideoMode(W, H), "SFML works!");
     window = new sf::RenderWindow(sf::VideoMode(W, H), "SFML works!");
@@ -66,12 +76,15 @@ struct Application
           window->close();
         if(event.type == sf::Event::MouseButtonPressed) {
           cout << "mouse pressed" << endl;
+          mouseDragging = 1;
+          // set the mouse clicked location
           sf::Vector2i position = sf::Mouse::getPosition();
-          cout << "x" << position.x << endl;
-          cout << "y" << position.y << endl;
+          mouse_down_col = position.x;
+          mouse_down_row = position.y;
         }
         if(event.type == sf::Event::MouseButtonReleased) {
           cout << "mouse released" << endl;
+          mouseDragging = 0;
         }
       }
       // update pixels
@@ -86,19 +99,36 @@ struct Application
         pixelgrid->pixels[i+3] = 255; // a
       }
 
+      int new_center_row = center_row;
+      int new_center_col = center_col;
+      if(mouseDragging) {
+        sf::Vector2i position = sf::Mouse::getPosition();
+        // cout << "x" << position.x << endl;
+        // cout << "y" << position.y << endl;
+        int delta_col = position.x - mouse_down_col;
+        int delta_row = position.y - mouse_down_row;
+
+        new_center_row = center_row + delta_row;
+        new_center_col = center_col + delta_col;
+
+        std::cout << "new_center_row" << " => " << new_center_row << std::endl;
+        std::cout << "new_center_col" << " => " << new_center_col << std::endl;
+
+        // std::cout << "center_row_delta" << " => " << center_row_delta << std::endl;
+        // std::cout << "center_col_delta" << " => " << center_col_delta << std::endl;
+      }
+
       // access by index
-      int row = 100;
-      for(int col=100; col < 120; col++) {
+      int row = new_center_row;
+      for(int col=new_center_col; col < new_center_col+20; col++) {
         (*pixelgrid)(row,col,0) = 255;
         (*pixelgrid)(row,col,1) = 0;
         (*pixelgrid)(row,col,2) = 0;
         (*pixelgrid)(row,col,3) = 255;
       }
 
-      // pixelgrid.pixels[4*W*row + 4*col + 0] = 0;
-      // pixelgrid.pixels[4*W*row + 4*col + 1] = 0;
-      // pixelgrid.pixels[4*W*row + 4*col + 2] = 0;
-      // pixelgrid.pixels[4*W*row + 4*col + 3] = 0;
+
+      // draw on screen
 
       texture->update((*pixelgrid).pixels);
 
