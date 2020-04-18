c369f9c4dd46d0a67b2c44c5deed6a33b9c628b0
jonathon <jonathonwhite@protonmail.com>
2020-04-18 17:51:51 +0200

    render a image to screen

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,28 @@ using namespace std;
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Red);
+  const unsigned int W = 200;
+  const unsigned int H = 200; // you can change this to full window size later
+
+  sf::RenderWindow window(sf::VideoMode(W, H), "SFML works!");
+  // sf::CircleShape shape(20.f);
+  // shape.setFillColor(sf::Color::Red);
+
+
+  sf::Uint8* pixels = new sf::Uint8[W*H*4];
+
+  sf::Texture texture;
+  texture.create(W, H); 
+  sf::Sprite sprite(texture); // needed to draw the texture on screen
+  // ...
+  for(register int i = 0; i < W*H*4; i += 4) {
+    pixels[i] = 255; // r
+    pixels[i+1] = 255; // g
+    pixels[i+2] = 1; // b
+    pixels[i+3] = 255; // a
+  }
+  texture.update(pixels);
+
 
   while (window.isOpen())
   {
@@ -20,9 +39,10 @@ int main()
     }
 
     window.clear();
-    window.draw(shape);
+    window.draw(sprite);
     window.display();
   }
+
 
   return 0;
 }
